46946e51691ab9a83f5111f5f425aa6cb16531d8
Kalvin Tang <ktang82@my.bcit.ca>
2021-06-17 04:15:58 -0700

    added loader gif

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import '../styles/header.css';
 
 const Header = () => {
     return (
-        <div>
-            <h1>Integer Facts</h1>
+        <div className="header">
+            <h1 className="title">Integer Facts</h1>
             <h3>Enter a number for a trivial fact for that number!</h3>
         </div>
     );
