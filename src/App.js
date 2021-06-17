e9e04ddcdd460cee63abb0ba2d5b11b53c1a748e
Kalvin Tang <ktang82@my.bcit.ca>
2021-06-17 09:03:59 -0700

    attempt to pass query input

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './styles/app.css';
 import Header from './components/Header';
 import Search from './components/Search';
 import ResultDisplay from './components/ResultDisplay';
 
 function App() {
+    const [data, setData] = useState([]);
+    const [query, setQuery] = useState('');
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios(`http://numbersapi.com/${query}/trivia`);
+            console.log(result.data);
+            setData(result.data);
+        };
+
+        fetchData();
+    }, [console.log(query)]);
     return (
         <div className="container">
             <Header />
-            <Search />
+            <Search input={(i) => setQuery(i)} />
             <ResultDisplay />
         </div>
     );
